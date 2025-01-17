80090f288d3ba78b1eaf124017e51c66df6f35b2
Chad Wilcox <chad.wilcox@truenorthenergypartners.com>
2025-01-17 14:53:07 -0500

    s

--- main.js
+++ main.js
@@ -41,37 +41,3 @@ document.addEventListener('DOMContentLoaded', () => {
         .catch(error => console.error('Error loading header:', error));
 });
 
-// Dynamic Featured Practice Section
-document.addEventListener('DOMContentLoaded', () => {
-    const practices = [
-        {
-            title: "LTSA Negotiation",
-            description: "TrueNorth specializes in assisting clients in the power industry with negotiating Long-Term Service Agreements (LTSA) to optimize operational performance and cost efficiency. Leveraging deep industry expertise, TrueNorth provides guidance on contract terms, pricing structures, and performance guarantees, ensuring alignment with client objectives.",
-            image: "images/LTSA.webp",
-            link: "practices.html"
-        },
-        {
-            title: "Rates and Regulatory",
-            description: "TrueNorth Energy Partners provides comprehensive support for utility rate cases, offering expert analysis and strategic guidance to navigate complex regulatory environments.",
-            image: "images/ScetionIIpc2.webp",
-            link: "practices.html"
-        }
-    ];
-
-    const randomPractice = practices[Math.floor(Math.random() * practices.length)];
-    const featuredPracticeSection = document.getElementById('featured-practice');
-    const practiceContent = document.getElementById('practice-content');
-
-    // Set background image for the section
-   if (featuredPracticeSection && practiceContent) {
-    featuredPracticeSection.style.backgroundImage = `url(${randomPractice.image})`;
-
-    practiceContent.innerHTML += `
-        <h3>${randomPractice.title}</h3>
-        <p>${randomPractice.description}</p>
-        <a href="${randomPractice.link}" class="btn btn-primary">Learn More</a>
-    `;
-} else {
-    console.error('Featured practice section or content container not found.');
-}
-});
